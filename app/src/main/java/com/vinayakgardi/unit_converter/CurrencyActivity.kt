6a724690b58195c72ef7057a5dac45ef876bdf23
Vinayak Gardi <vinayakgardi7@gmail.com>
2023-02-18 07:03:37 +0530

    Completed Length and Currency Screens

--- app/src/main/java/com/vinayakgardi/unit_converter/CurrencyActivity.kt
+++ app/src/main/java/com/vinayakgardi/unit_converter/CurrencyActivity.kt
@@ -1,11 +1,37 @@
 package com.vinayakgardi.unit_converter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.text.Typography.dollar
+import kotlin.text.Typography.euro
 
 class CurrencyActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency)
+//
+        val convert : Button = findViewById(R.id.currencyConvert)
+        val cedittext : EditText = findViewById(R.id.cedittext)
+        val currencytext : TextView = findViewById(R.id.currencytext)
+
+
+//
+        convert.setOnClickListener {
+            val rupee : Double =  cedittext.text.toString().toDouble()
+            val dollar : Double = rupee * 0.012
+            val euro  : Double = rupee *0.011
+            val egyptianpound : Double = rupee* 0.37
+            val dihram : Double = rupee *0.044
+            val jpnyen : Double = rupee * 1.63
+              currencytext.append("$dollar\n")
+            currencytext.append("$euro\n")
+        }
+//
     }
 }
--- app/src/main/java/com/vinayakgardi/unit_converter/LengthActivity.kt
+++ app/src/main/java/com/vinayakgardi/unit_converter/LengthActivity.kt
@@ -2,10 +2,41 @@ package com.vinayakgardi.unit_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LengthActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_length)
+
+       val convert: Button = findViewById(R.id.lengthConvert)
+       val lengthtext: TextView = findViewById(R.id.Lengthtext)
+       val edittext: EditText = findViewById(R.id.ledittext)
+
+
+
+        convert.setOnClickListener {
+            val m =  edittext.text.toString()
+            var meter : Double = m.toDouble()
+            val cm : Double= meter * 100.00
+            val inch : Double =meter *39.3701
+            val foot : Double = meter * 3.28084
+            val km : Double = meter * 0.001
+            val mile : Double = meter *0.000621371
+            val mm : Double = meter * 1000.0
+
+
+
+        }
+
+
     }
 }
+
+
+
